226ffb275f4f08e39a87d7d8cea062a30b344d18
MGSimard <mgsimard.dev@gmail.com>
2026-03-01 20:03:34 -0500

    .output instead of dist

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,5 +1,2 @@
-import { clientEnv } from "@/env.client";
-
-export const SITE_URL = clientEnv.VITE_SITE_URL;
 export const SITE_TITLE = "Tanstack AWS";
 export const SITE_DESCRIPTION = "Experimentation with Tanstack Start & AWS infrastructure";
