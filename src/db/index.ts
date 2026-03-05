3167d014ea3ed72dbdaff84f42a2c93e9d4ced08
MGSimard <mgsimard.dev@gmail.com>
2026-03-05 11:45:21 -0500

    a

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema.ts";
-import { serverEnv } from "@/env.server.ts";
+import { serverEnv } from "@/env-server.ts";
 
 export const db = drizzle(serverEnv.DATABASE_URL, { schema });
