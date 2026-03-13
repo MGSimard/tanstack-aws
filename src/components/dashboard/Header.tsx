90533fdf047d4f347dbeb4b31cb236f12f994313
MGSimard <mgsimard.dev@gmail.com>
2026-03-13 01:50:09 -0400

    Restore mr-2 to mirror space from button on left of separator

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -14,7 +14,7 @@ export function Header() {
     <header className="sticky inset-x-0 top-0 z-10 flex h-16 shrink-0 items-center gap-2 bg-sidebar px-6 py-4 md:px-8">
       <div className="flex items-center gap-2">
         <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="h-4 data-vertical:self-center" />
+        <Separator orientation="vertical" className="mr-2 h-4 data-vertical:self-center" />
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem className="hidden md:block">
