bbd2de580c64d8464d09a643dba46970ea347bd6
MGSimard <mgsimard.dev@gmail.com>
2026-03-13 10:26:16 -0400

    Vite 8 + updates

--- README_VITE8.md
+++
@@ -1,4 +0,0 @@
-Migrate to Vite 8 once their release isn't utterly dogshit and filled with even the most basic IDE & type errors that would be found within 5 seconds of testing their own implementation examples.
-
-https://vite.dev/blog/announcing-vite8
-https://github.com/vitejs/vite-plugin-react/releases/tag/plugin-react%406.0.0
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { nitro } from "nitro/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
-  plugins: [
-    devtools(),
-    nitro(),
-    tsconfigPaths({ projects: ["./tsconfig.json"] }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact({
-      babel: {
-        plugins: ["babel-plugin-react-compiler"],
-      },
-    }),
-  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [devtools(), nitro(), tanstackStart(), react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
 });
 
 export default config;
